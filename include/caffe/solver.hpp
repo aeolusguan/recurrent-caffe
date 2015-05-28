e2d4bf4f312ffc8ee7243a8793eca640129f4e1f
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2015-05-28 12:03:02 -0500

    Added set iter method

--- include/caffe/solver.hpp
+++ include/caffe/solver.hpp
@@ -37,17 +37,17 @@ class Solver {
     return test_nets_;
   }
   int iter() { return iter_; }
-
+  void set_iter(int iter) { iter_ = iter; }
   // The Solver::Snapshot function implements the basic snapshotting utility
   // that stores the learned net. You should implement the SnapshotSolverState()
   // function that produces a SolverState protocol buffer that needs to be
   // written to disk together with the learned net.
   void Snapshot();
   void Snapshot(const std::string& snapshot_prefix);
-
- protected:
   // Get the update value for the current iteration.
   virtual void ComputeUpdateValue() = 0;
+
+ protected:
   // The test routine
   void TestAll();
   void Test(const int test_net_id = 0);
